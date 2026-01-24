60681e7bc61b75009f0085105a394be29f6a3ffa
ARK <arkhan.public@gmail.com>
2026-01-25 02:40:10 +0600

    Trying to push to git hub from VS using co pilot

---
+++ Repositories/Interfaces/IPurchaseOrderRepository.cs
@@ -0,0 +1,14 @@
+using InventorySystem.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InventorySystem.Repositories.Interfaces
+{
+    public interface IPurchaseOrderRepository
+    {
+        Task<IEnumerable<PurchaseOrder>> GetAllWithItemsAsync();
+        Task<PurchaseOrder?> GetByIdWithItemsAsync(int id);
+        Task AddAsync(PurchaseOrder order);
+        Task AddItemAsync(PurchaseOrderItem item);
+    }
+}
---
+++ Repositories/Interfaces/ISalesOrderRepository.cs
@@ -0,0 +1,14 @@
+using InventorySystem.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InventorySystem.Repositories.Interfaces
+{
+    public interface ISalesOrderRepository
+    {
+        Task<IEnumerable<SalesOrder>> GetAllWithItemsAsync();
+        Task<SalesOrder?> GetByIdWithItemsAsync(int id);
+        Task AddAsync(SalesOrder order);
+        Task AddItemAsync(SalesOrderItem item);
+    }
+}
